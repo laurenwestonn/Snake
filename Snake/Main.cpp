0a941961f1652816d521a7921a86d5ee38518c56
laurenweston <lauren-weston@hotmail.co.uk>
2018-11-27 19:50:16 +0000

    Make santa jiggle
    Textures and sprites

--- Snake/Main.cpp
+++ Snake/Main.cpp
@@ -4,7 +4,20 @@ int main(int argc, char** argv[])
 {
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Boop");
 
+	sf::Texture texture;
+	if (!texture.loadFromFile("santa.jpg"))
+		printf("Couldn't load jpg");
+
+	sf::Sprite santa(texture);
+	santa.setPosition(320, 240);
+	santa.setOrigin(texture.getSize().x / 2, texture.getSize().y / 2);
+
+	float xDir = 0.4;
+	float yDir = 0.4;
+
 	while (window.isOpen()) {
+		window.clear(sf::Color::Black);
+
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
@@ -12,12 +25,20 @@ int main(int argc, char** argv[])
 			}
 		}
 
-		sf::RectangleShape rect = sf::RectangleShape(sf::Vector2f(200, 170));
-		rect.setFillColor(sf::Color(255, 190, 210, 255));
-		rect.setPosition(100, 100);
+		if ((santa.getPosition().x - (texture.getSize().x / 2) + xDir) < 0 ||
+			(santa.getPosition().x + (texture.getSize().x / 2) + xDir) > window.getSize().x) {
+			xDir *= -1;
+			santa.setColor(sf::Color(255, 100, 100, 255));
+		}
 
-		window.clear(sf::Color::Black);
-		window.draw(rect);
+		if ((santa.getPosition().y - (texture.getSize().y / 2) + yDir) < 0 ||
+			(santa.getPosition().y + (texture.getSize().y / 2) + yDir) > window.getSize().y) {
+			yDir *= -1;
+			santa.setColor(sf::Color(100, 100, 255, 255));
+		}
+
+		santa.setPosition(santa.getPosition().x + xDir, santa.getPosition().y + yDir);
+		window.draw(santa);
 		window.display();
 	}
 }
