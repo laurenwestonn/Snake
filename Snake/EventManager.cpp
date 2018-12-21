fa29267e6cdfd5347f3c5654dee5766424b3869f
Lauren Weston <lauren.weston@webapplicationsuk.com>
2018-12-21 17:22:52 +0000

    Copied and pasted in handle event code from page 82
    Not checked, should be ok. try to understand

--- Snake/EventManager.cpp
+++ Snake/EventManager.cpp
@@ -37,3 +37,49 @@ void EventManager::SetFocus(const bool & l_focus)
 {
 	m_hasFocus = l_focus;
 }
+
+void EventManager::HandleEvent(sf::Event & l_event)
+{
+	// Iterate through each binding & event type
+	for (auto &b_itr : m_bindings) {
+		Binding* bind = b_itr.second;    
+		for (auto &e_itr : bind->m_events) {
+			EventType sfmlEvent = (EventType)l_event.type;      
+			if (e_itr.first != sfmlEvent) { continue; }      
+			
+			if (sfmlEvent == EventType::KeyDown || sfmlEvent == EventType::KeyUp) {
+				if (e_itr.second.m_code == l_event.key.code) {          
+					// Matching event/keystroke.          
+					// Increase count.          
+					if (bind->m_details.m_keyCode != -1){            
+						bind->m_details.m_keyCode = e_itr.second.m_code;          
+					}          
+					++(bind->c);          
+					break;        
+				}      
+			} else if (sfmlEvent == EventType::MButtonDown || sfmlEvent == EventType::MButtonUp) {
+					if (e_itr.second.m_code == l_event.mouseButton.button) {          
+						// Matching event/keystroke.          
+						// Increase count.          
+						bind->m_details.m_mouse.x = l_event.mouseButton.x;         
+						bind->m_details.m_mouse.y = l_event.mouseButton.y;      
+						if (bind->m_details.m_keyCode != -1){         
+							bind->m_details.m_keyCode = e_itr.second.m_code;     
+						}        
+						++(bind->c);      
+						break;      
+					}   
+			} else {    
+				// No need for additional checking.   
+				if (sfmlEvent == EventType::MouseWheel){    
+					bind->m_details.m_mouseWheelDelta = l_event.mouseWheel.delta;      
+				} else if (sfmlEvent == EventType::WindowResized){        
+					bind->m_details.m_size.x = l_event.size.width;       
+					bind->m_details.m_size.y = l_event.size.height;     
+				} else if (sfmlEvent == EventType::TextEntered){     
+					bind->m_details.m_textEntered = l_event.text.unicode;   
+				}      
+				++(bind->c);  
+			}   
+		} 
+}
